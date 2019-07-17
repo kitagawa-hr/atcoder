ac58aa75209b3cfd33b436bc4a994d5c39f336b7
kitagawa-hr <kitagawa@cancerscan.jp>
2019-07-17 14:55:11 +0900

    Solve abc113

---
+++ abc113/A/main.kt
@@ -0,0 +1,25 @@
+import java.io.*
+import java.util.*
+
+fun solve(X: Long, Y: Long) {
+    println(X + Y / 2L)
+    return
+}
+
+fun main(args: Array<String>) {
+    fun StringArray(size: Int, init: (Int) -> String = { "\u0000" }): Array<String> {
+        return Array<String>(size, init)
+    }
+    class Scanner(stream: InputStream) {
+        private val reader = BufferedInputStream(stream).bufferedReader()
+        private var st: StringTokenizer? = null
+        fun next(): String {
+            while (!(st?.hasMoreTokens() ?: false)) st = StringTokenizer(reader.readLine())
+            return st!!.nextToken()
+        }
+    }
+    val sc = Scanner(System.`in`)
+    val X = sc.next().toLong()
+    val Y = sc.next().toLong()
+    solve(X, Y)
+}
