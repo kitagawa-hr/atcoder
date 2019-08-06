f05a518256b3e8604d95c5a64e9660d5de55d506
kitagawa-hr <kitagawa@cancerscan.jp>
2019-08-06 11:34:26 +0900

    Solve WeightedUnionFind problems

--- abc126/D/main.kt
+++ abc126/D/main.kt
@@ -20,44 +20,44 @@ fun solve(N: Int, u: IntArray, v: IntArray, w: IntArray) {
     return
 }
 
-
 class WeightedUnionFind(n: Int) {
-    data class Node(var parent: Int, var size: Int, var weightDiff: Long)
+    data class Node(var parent: Int, var size: Int, var weight: Long)
 
-    private val nodes = Array(n) { Node(parent = it, size = 1, weightDiff = 0) }
+    private val nodes = Array(n) { Node(parent = it, size = 1, weight = 0) }
     fun findRoot(x: Int): Int {
         if (x == this.nodes[x].parent) return x
         val root = findRoot(nodes[x].parent)
-        nodes[x].weightDiff += nodes[nodes[x].parent].weightDiff
+        nodes[x].weight += nodes[nodes[x].parent].weight
         nodes[x].parent = root
         return root
     }
 
     fun unite(x: Int, y: Int, weight: Long): Boolean {
+        // nodes[x].weight + weight = nodes[y].weightとなるようにする
         val xRoot = findRoot(x)
         val yRoot = findRoot(y)
         if (xRoot == yRoot) return false
-        val (smaller, bigger) = if (getSize(xRoot) < getSize(yRoot)) {
-            Pair(xRoot, yRoot)
-        } else {
-            Pair(yRoot, xRoot)
+        val (smaller, bigger, newWeight) = when (getSize(xRoot) >= getSize(yRoot)) {
+            true -> Triple(yRoot, xRoot, nodes[x].weight - nodes[y].weight + weight)
+            else -> Triple(xRoot, yRoot, nodes[y].weight - nodes[x].weight - weight)
         }
-        val newWeight = weight + getWeight(xRoot) - getWeight(yRoot)
         nodes[smaller].parent = bigger
         nodes[bigger].size += nodes[smaller].size
-        nodes[smaller].weightDiff = if (smaller == xRoot) -newWeight else newWeight
+        nodes[smaller].weight = newWeight
         return true
     }
 
     fun getWeight(x: Int): Long {
         findRoot(x)
-        return nodes[x].weightDiff
+        return nodes[x].weight
     }
 
     fun getDiff(x: Int, y: Int) = getWeight(y) - getWeight(x)
     fun getSize(x: Int) = nodes[findRoot(x)].size
     fun isSame(x: Int, y: Int) = findRoot(x) == findRoot(y)
 }
+
+
 
 
 fun main(args: Array<String>) {
